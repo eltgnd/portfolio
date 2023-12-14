6cfe7f70033ae94940926be88ada73356286e95e
Val Eltagonde <valeltagonde@gmail.com>
2023-12-14 22:08:14 +0800

    first commit

---
+++ info_txt.py
@@ -0,0 +1,3 @@
+about_txt = """
+Hi there I'm Val. A self-motivated and passionate data science sophomore at Ateneo de Manila University. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+"""
